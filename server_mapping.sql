31e536054508d58f31477a1a99702636a3c1250b
Winston <63299212+winstonrebello@users.noreply.github.com>
2024-05-11 15:30:21 +0530

    Create server_mapping.sql

---
+++ server_mapping.sql
@@ -0,0 +1,33 @@
+-- To create server and store host,port and dbname
+CREATE SERVER remote_server
+    FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (host (
+                   SELECT host_name 
+                   FROM dim.encrypted_connections 
+                   WHERE id = 2
+                ), 
+             port (
+                   SELECT port_no 
+                   FROM dim.encrypted_connections 
+                   WHERE id = 2
+                ), 
+             dbname (
+                   SELECT db_name 
+                   FROM dim.encrypted_connections 
+                   WHERE id = 2
+                )
+            );
+
+-- To create mapping which contain username and password
+CREATE USER MAPPING FOR postgres
+    SERVER remote_server
+    OPTIONS (user (
+                   SELECT username 
+                   FROM dim.encrypted_connections 
+                   WHERE id = 2
+                ), password (
+                   SELECT password 
+                   FROM dim.encrypted_connections 
+                   WHERE id = 2
+                )
+            );
